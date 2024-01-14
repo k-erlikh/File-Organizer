b5567c2ff6971f613e18be95a177f3a75bdb6c30
k-erlikh <kateerlikh@cmail.carleton.ca>
2024-01-14 16:25:35 -0500

    Updated pdfToText to catch IOException error, created method to iterate over pdf string format

--- fileorganizer/src/main/java/FileFrequency.java
+++ fileorganizer/src/main/java/FileFrequency.java
@@ -3,6 +3,8 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -11,7 +13,13 @@ public class FileFrequency{
 
     private static final String regex = "[!._,@? ]";
 
-    public static Frequencies iterateFile(BufferedReader r, PrintWriter w){
+    /*
+     * Method: Iterates Over Text File
+     * Purpose: Reads through every word in a text file, looking for key words. If a key word is found,
+     * it adds to the frequency of the HashMap. Once the file has ended, based on the end results, the
+     * HashMap will be analyzed and the file will be sorted.
+     */
+    public static Frequencies iterateTextFile(BufferedReader r, PrintWriter w){
         try{
             Frequencies frequency = new Frequencies();
 
@@ -36,14 +44,48 @@ public class FileFrequency{
         return null;
     }
     
-    public static void createDirectories(){
+    /*
+     * Method: Pick Directory
+     * Purpose: Based on the HashMap frequencies, this method will determine which directory it should be sorted in.
+     *  If the directory hasn't been created yet, it will create the directory. Otherwise it will add it to the given 
+     * directory.
+     */
+    public static void pickDirectory(Frequencies freq){
         return;
     }
+    /*
+     * Method: PDF to Text
+     * Purpose: Transforms the PDF contents into a String. Uses apache PDFBox dependencies to acheive this. 
+     */
+    public static String pdfToText(File path){
+        try{
+            PDDocument pdf = PDDocument.load(path);
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String textContent = pdfStripper.getText(pdf);
+            return textContent;
+        }
+        catch(IOException error){
+            System.out.println(error);
+        }
+        return null;
+    }
 
-    public static String pdfToText(String path){
-        PDDocument pdf = PDDocument.load(path);
-        PDFTextStripper pdfStripper = new PDFTextStripper();
-        String textContent = pdfStripper.getText(pdf);
-        return textContent;
+    /*
+     * Method: Iterate PDF Text
+     * Purpose: Splits the String returned by the pdf conversion function, and reads over every word, looking for key words.
+     */
+    public static Frequecies iteratePdfText(String text){
+        if (!text.isEmpty()){
+            Frequencies frequency = new Frequencies();
+            String[] words = text.split(" ");
+            for (String word:words){
+                if (frequency.words.containsKey(word)){
+                    frequency.words.put(word, frequency.words.get(word) + 1);
+                }
+            }
+            return frequency;  
+        }
+        System.out.println("Error: String/PDF file provided is empty.");
+        return null;
     }
 }
